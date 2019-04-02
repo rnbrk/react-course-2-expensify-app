a34d31af90f57b19a0284b13eae0b378e651b2db
Ron Broek <r.t.broek@hotmail.com>
2019-04-02 13:26:24 +0200

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git commands
+
+git init - Create new git repo
+git status - View changes to your project code
+git add - Add files to staging area
+git commit - Creates a new commit with files from the staging area
+git log - View recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,55 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [{
-      // Will use babel on js files
-      loader: 'babel-loader',
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-      // Only .js files that are not in node_modules folder
-      test: /\.js$/,
-      exclude: /node_modules/,
+  console.log('env', env);
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    {
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-      test: /\.s?css$/,
-    }],
-  },
-  // Used when debugging: so when there's an error
-  // you see the line in the original source file and not bundle.js!
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-  },
+    module: {
+      rules: [
+        {
+          // Will use babel on js files
+          loader: 'babel-loader',
+
+          // Only .js files that are not in node_modules folder
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    // Used when debugging: so when there's an error
+    // you see the line in the original source file and not bundle.js!
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
