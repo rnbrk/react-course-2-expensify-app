b24dbd6323c81d68e490236604a6190df372d155
Ron Broek <r.t.broek@hotmail.com>
2019-04-09 08:51:36 +0200

    Removed Firebase reference

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -26,16 +26,22 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
 // REMOVE_EXPENSE
-
 export const removeExpense = id => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
 // EDIT_EXPENSE
-
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = expenses => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+// export const startSetExpenses;
