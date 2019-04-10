fd7b8ebe8304e942c313d978bbe6c8feb51a8083
Ron Broek <r.t.broek@hotmail.com>
2019-04-10 11:37:37 +0200

    Added option to switch Google accounts

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,4 +14,8 @@ firebase.initializeApp(config);
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
+googleAuthProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+
 export { firebase, googleAuthProvider, database as default };
